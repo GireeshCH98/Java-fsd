af5f027acb00d28980ea918bc359aa089242944b
Suhana R Ghasi <suhanarg.mca19@rvce.edu.in>
2024-03-29 09:50:11 +0530

    Add Fill to Github

---
+++ src/com/phase1/PracticeProject11/MyThread.java
@@ -0,0 +1,16 @@
+package com.phase1.PracticeProject11;
+
+public class MyThread extends Thread
+{
+	public void run() 
+	{
+		System.out.println("Thread using extends is running.");
+	}
+
+	public static void main(String[] args) 
+	{
+		MyThread t = new  MyThread();
+  		t.start();
+	}
+
+}
