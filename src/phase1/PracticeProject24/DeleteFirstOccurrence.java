f2b744b03bb5eed08c1fca4b77eb738a7d01b7a2
Gireesh C Hosakoppa <83752497+GireeshCH98@users.noreply.github.com>
2024-04-01 20:57:41 +0530

    Phase-1 Practice Project: Assisted Practice- 24

---
+++ src/phase1/PracticeProject24/DeleteFirstOccurrence.java
@@ -0,0 +1,89 @@
+//Write a program in Java to delete the first occurrence of a key in a singly linked list
+package phase1.PracticeProject24;
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    Node head;
+
+    public SinglyLinkedList() {
+        this.head = null;
+    }
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public void delete(int key) {
+        Node temp = head;
+        Node prev = null;
+
+        // If key is found at head
+        if (temp != null && temp.data == key) {
+            head = temp.next;
+            return;
+        }
+
+        // Search for the key to be deleted
+        while (temp != null && temp.data != key) {
+            prev = temp;
+            temp = temp.next;
+        }
+
+        // If key is not present in the list
+        if (temp == null) {
+            System.out.println("Key not found in the list");
+            return;
+        }
+
+        // Unlink the node containing the key
+        prev.next = temp.next;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
+
+public class DeleteFirstOccurrence {
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.insert(5);
+
+        System.out.println("Original List:");
+        list.display();
+
+        int key = 3;
+        System.out.println("Deleting first occurrence of key " + key);
+        list.delete(key);
+
+        System.out.println("List after deletion:");
+        list.display();
+    }
+}
