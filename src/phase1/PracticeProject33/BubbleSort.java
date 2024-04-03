64aebdcf1ca3aa3267fb6bae2bca3bb4ff670c93
Gireesh C Hosakoppa <83752497+GireeshCH98@users.noreply.github.com>
2024-04-03 20:08:57 +0530

    Phase-1 Practice Project: Assisted Practice- 33

---
+++ src/phase1/PracticeProject33/BubbleSort.java
@@ -0,0 +1,36 @@
+package phase1.PracticeProject33;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        bubbleSort(arr);
+        System.out.println("The sorted array is:");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap the elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
