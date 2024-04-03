2b56e31aa0a2260d5fda8620e4756417e286bbf5
Gireesh C Hosakoppa <83752497+GireeshCH98@users.noreply.github.com>
2024-04-03 20:09:21 +0530

    Phase-1 Practice Project: Assisted Practice- 34

---
+++ src/phase1/PracticeProject34/InsertionSort.java
@@ -0,0 +1,35 @@
+package phase1.PracticeProject34;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        insertionSort(arr);
+        System.out.println("The sorted array is:");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
