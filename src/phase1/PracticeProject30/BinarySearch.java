1bf4c31c2c52eb381841b61682d2f5e2b30eb460
Gireesh C Hosakoppa <83752497+GireeshCH98@users.noreply.github.com>
2024-04-03 20:07:35 +0530

    Phase-1 Practice Project: Assisted Practice- 30

---
+++ src/phase1/PracticeProject30/BinarySearch.java
@@ -0,0 +1,38 @@
+package phase1.PracticeProject30;
+
+import java.util.Scanner;
+
+class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        System.out.print("Enter the value to search for: ");
+        int target = sc.nextInt();
+        int result = binarySearch(arr, 0, n - 1, target);
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+
+    static int binarySearch(int[] arr, int l, int r, int x) {
+        if (r >= l) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == x)
+                return mid;
+            if (arr[mid] > x)
+                return binarySearch(arr, l, mid - 1, x);
+            return binarySearch(arr, mid + 1, r, x);
+        }
+        return -1;
+    }
+}
